7b9497cbe529b4707045a2f264c90190d4bace2b
Firhand Hawarie <55737624+warrexx@users.noreply.github.com>
2023-09-26 22:12:15 +0900

    Editted format

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,31 @@
 def arithmetic_arranger(problems):
-
+    
+    if problems.size > 5:
+      print('Error: Too many problems')
+      
+    for problem in problems :
+      top = r'(\d+)\s*[\+\-\:\*\/]'
+      bottom = r'(\d+)\s*[\+\-\:\*\/]'
+      operator = x
+      
+      if operator || '+' or operator || '-' cond:
+        print('Error: Operator must be '+' or '-'')
+        break
+      
+      elif top.type() || int or top.type() || int:
+        print('Error: Numbers must only contain digits.')
+        break
+      
+      elif top.len() > 4 or bottom.len() > 4 :
+        print('Error: Numbers cannot be more than four digits.')  
+        break
+      
+      match operator:
+        case '+':
+          result = top + bottom
+        case '-':
+          result = top - bottom  
+          
+    arranged_problems= dict()
 
     return arranged_problems
